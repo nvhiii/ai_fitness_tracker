0ac16e0380715b2e4fd11cf10662b141798a863f
Aaleia <aaleia.fernando@gmail.com>
2024-01-13 14:02:12 -0500

    Added variables for user demographics

--- scripts/backend.js
+++ scripts/backend.js
@@ -1,22 +1,39 @@
 const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
-
+//global.fetch = require('node-fetch');
 
 const MODEL_NAME = "gemini-pro";
 const API_KEY = "AIzaSyDf0vX1frSdozgaOtOzmS5tb8ZNtt7T95Q";
 
 async function run() {
-  const genAI = new GoogleGenerativeAI(API_KEY);
-  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
-  prompt = "Make an exercise routine for a female who is 150-180 pounds and ages 25-32 is 5 foot 5 inches. She is new to fitness and has one hour to allocate per day."
-  const result = await model.generateContentStream(prompt);
+    const gender = "female";
+    const weight_class = "150-180 pounds";
+    const age_range = "25-32";
+    const height = "5 foot 5 inches";
+    const experience = "new to fitness";
+    const time_available = "one hour per day";
 
-  let text = '';
-  for await (const chunk of result.stream) {
-    const chunkText = chunk.text();
-    console.log(chunkText);
-    text += chunkText;
-  }
+    const prompt = `Create a strength-training routine for a ${age_range} year-old ${gender} who weighs ${weight_class} and is ${height} tall. They are ${experience} and have ${time_available} to allocate per day.`;
+    
+    // Streaming
+    const result = await model.generateContentStream(prompt);
+
+    let text = '';
+    for await (const chunk of result.stream) {
+	const chunkText = chunk.text();
+	console.log(chunkText);
+	text += chunkText;
+    }
+
+    // Without streaming
+    /*
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+    */
 
 }
 
